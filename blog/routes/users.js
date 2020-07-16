0d3197426586930f268097b7652acc135b025b90
shixian <1145024061@qq.com>
2020-07-16 18:28:14 +0800

    07.16

--- blog/routes/users.js
+++ blog/routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+var User = require('../models/user.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// 管理员登录
+router.post('/adminLogin',(req,res,next)=>{
+	var userName = req.body.user,
+		password = req.body.pwd;
+	User.findOne({userName:userName},(loginErr,loginDoc)=>{
+		// console.log(loginDoc);
+		if(loginDoc){
+			if(loginDoc.password == password){
+				return res.json({
+					code:0,
+					msg:'登录成功！',
+					result:	''
+				});
+				
+			}else{
+				return res.json({
+					code:1,
+					msg:'密码错误,请重试！',
+					result:''	
+				})
+			}
+		}
+	})
+	
+})
 
 module.exports = router;
